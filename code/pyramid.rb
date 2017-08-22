e215e0ad417f1e857aafbdf540f132667634c7d8
Luv <lsharma@grepruby.com>
2017-08-22 16:11:15 +0530

    initial code structure is to return left, right or center pyramid with n rows

---
+++ code/pyramid.rb
@@ -0,0 +1,25 @@
+class Pyramid
+
+  def initialize(n)
+ 	# initialize number of rows
+  	@rows = n
+  end
+
+  
+  # return left pyramid with n rows
+  def left_align
+   # code for left align pyramid
+  end
+
+
+  # return right pyramid with n rows
+  def right_align
+   # code for right align pyramid
+  end
+
+
+  # return center pyramid with n rows
+  def center_align
+  # code for center align pyramid
+  end  
+end
---
+++ test/tc_pyramid.rb
@@ -0,0 +1,21 @@
+# File : tc_pyramid.rb
+
+require_relative "../code/pyramid"
+require "test/unit"
+
+class TestPyramid
+
+  def test_left
+  # code to execute left_align function
+  end
+
+  def test_right
+  # code to execute right_align function
+  end
+
+
+  def test_center
+  # code to execute center_align function
+  end
+
+end
