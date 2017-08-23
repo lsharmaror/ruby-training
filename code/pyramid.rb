ff77508b4529c1adb661d0ccea034c32dec9ccb3
Luv <lsharma@grepruby.com>
2017-08-23 11:15:27 +0530

    Revised program

--- code/pyramid.rb
+++ code/pyramid.rb
@@ -3,27 +3,30 @@ class Pyramid
     # initialize number of rows
     @rows = n
   end
+
   # return left pyramid with n rows
   def left_align
-    pattern = ""
+    pattern = ''
     (1..@rows).each do |row|
-      pattern += (('*' * row)+ "\n")
+      pattern += (('*' * row) + "\n")
     end
     pattern.rstrip
   end
+
   # return right pyramid with n rows
   def right_align
-    pattern = ""
+    pattern = ''
     (1..@rows).each do |row|
-      pattern += (('*' * row).rjust(@rows)+ "\n")
+      pattern += (('*' * row).rjust(@rows) + "\n")
     end
     pattern.rstrip
   end
+
   # return center pyramid with n rows
   def center_align
     pattern = ""
     (1..@rows).each do |row|
-       pattern += (('* ' * row).center(@rows*2).rstrip + "\n")
+      pattern += (('* ' * row).center(@rows*2).rstrip + "\n")
     end
     pattern.rstrip
   end
--- test/tc_pyramid.rb
+++ test/tc_pyramid.rb
@@ -1,24 +1,21 @@
 # File : tc_pyramid.rb
-
-require_relative "../code/pyramid"
-require "test/unit"
+require_relative '../code/pyramid'
+require 'test/unit'
 
 class TestPyramid < Test::Unit::TestCase
-
   def test_left
     expected = "*\n**\n***\n****\n*****"
-    assert_equal(expected,Pyramid.new(5).left_align)
+    assert_equal(expected, Pyramid.new(5).left_align)
   end
 
   def test_right
     expected = "    *\n   **\n  ***\n ****\n*****"
-    assert_equal(expected,Pyramid.new(5).right_align)
+    assert_equal(expected, Pyramid.new(5).right_align)
   end
 
 
   def test_center
     expected = "    *\n   * *\n  * * *\n * * * *\n* * * * *"
-  assert_equal(expected,Pyramid.new(5).center_align)
+    assert_equal(expected, Pyramid.new(5).center_align)
   end
-
 end
