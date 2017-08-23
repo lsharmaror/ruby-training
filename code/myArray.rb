ffb6e1d63b62755f0fec2dd8d91e0529bb7407ad
lsharmaror <31235474+lsharmaror@users.noreply.github.com>
2017-08-23 19:56:27 +0530

    Update myArray.rb

--- code/myArray.rb
+++ code/myArray.rb
@@ -1,20 +1,20 @@
 class MyArray
   def initialize(arr)
-   @my = arr
+    @my = arr
   end
 
   def frequency
-   a = Hash.new(0)
-   @my.map{ |i| a[i] += 1}
-   a
+    a = Hash.new(0)
+    @my.map { |i| a[i] += 1 }
+    a
   end
 
   def twice
-   @my.map { |a| a.is_a?(Numeric) ? a * 2 : a }
+    @my.map { |a| a.is_a?(Numeric) ? a * 2 : a }
   end
 
   def join(a)
-   @ar = a
-   @my.concat(a)
+    @ar = a
+    @my.concat(a)
   end
 end
