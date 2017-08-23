c0dde283363f66ee190948c5a628b51f2c4d3569
Luv <lsharma@grepruby.com>
2017-08-23 19:49:12 +0530

    Final code

--- test/tc_class_Array.rb
+++ test/tc_class_Array.rb
@@ -3,20 +3,19 @@
 require_relative "../code/myArray"
 require "test/unit"
 
-class TestArray
+class TestArray < Test::Unit::TestCase
+  def test_frequency
+   expected = {1=>2, "3"=>1, 2=>1}
+   assert_equal(expected, MyArray.new([1, "3", 1, 2]).frequency)
+  end
 
- def test_frequency
-  expected = "1=>2, "3"=>1, 2=>1"
-  assert_equal(expected, myArray.new("1, "3", 1, 2").frequency)
- end
+  def test_twice
+   expected = [2, "3", 2, 4]
+   assert_equal(expected, MyArray.new([1, "3", 1, 2]).twice)
+  end
 
- def test_twice
-  expected = "2, 2, 4"
-  assert_equal(expected, myArray.new("1, "3", 1, 2").twice)
- end
-
- def test_join
-  expected = "1, "3", 1, 2, 4, 5, 6"
-  assert_equal(expected, myArray.new("1, "3", 1, 2").join("4, 5, 6"))
- end
+  def test_join
+   expected = [1, "3", 1, 2, 4, 5, 6]
+   assert_equal(expected, MyArray.new([1, "3", 1, 2]).join([4, 5, 6]))
+  end
 end
