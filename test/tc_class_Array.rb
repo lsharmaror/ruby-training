1289916504f1990b61b665f6e32215222c3a8c51
Luv <lsharma@grepruby.com>
2017-08-23 13:48:47 +0530

    Array_struc

---
+++ test/tc_class_Array.rb
@@ -0,0 +1,19 @@
+# File: tc_class_Array.rb
+
+require_relative "../code/class_Array"
+require "test/unit"
+
+class TestArray
+
+ def test_frequency
+  #code to execute frequency function
+ end
+
+ def test_twice
+  # code to execute twice function
+ end
+
+ def test_join
+  # code to execute join function
+ end
+end
